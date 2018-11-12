595d45f04a9367284d256e263ad4d04ceaf02047
ChenZiLong <qasw77564@gmail.com>
2018-11-12 15:32:52 +0800

    no message

--- KotlinApplication/app/src/main/java/evan/idv/kotlinapplication/main/tab/DashboardFragment.kt
+++ KotlinApplication/app/src/main/java/evan/idv/kotlinapplication/main/tab/DashboardFragment.kt
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import evan.idv.kotlinapplication.ApiUrl
 import evan.idv.kotlinapplication.R
 
@@ -36,7 +38,24 @@ class DashboardFragment : Fragment() {
     }
 
     fun getViews(view : View){
-
+//        val i : Int = 0
+        val c : Int = 1
+        var count : Int = 0
+        val textView2 : TextView = view.findViewById(R.id.dashhText)
+        val textView : TextView = view.findViewById(R.id.dashText)
+        val btn : Button = view.findViewById(R.id.dashBtn)
+        btn.setOnClickListener(View.OnClickListener {
+            count += c
+            textView.setText(count.toString())
+            for (i in 1..10){
+                count += i
+                textView2.setText(count.toString())
+            }
+        })
+        textView.setOnClickListener(View.OnClickListener {
+            count = 0
+            textView.setText(count.toString())
+        })
     }
 
 }
