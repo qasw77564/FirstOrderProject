da8b5b2792e82cac9c16ff7b7435cbbe6c70912a
ZiLong Chen <qasw77564@gmail.com>
2018-11-21 13:41:39 +0800

    no message

--- KotlinApplication/app/src/main/java/evan/idv/kotlinapplication/menu/CameraFragment.kt
+++ KotlinApplication/app/src/main/java/evan/idv/kotlinapplication/menu/CameraFragment.kt
@@ -1,6 +1,7 @@
 package evan.idv.kotlinapplication.menu
 
 
+import android.content.Context
 import android.media.Image
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -11,9 +12,23 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 import evan.idv.kotlinapplication.R
 
 class CameraFragment : Fragment() {
+
+    companion object {
+        private lateinit var context : Context
+        //google login
+        private val RC_SIGN_IN = 1144
+        private lateinit var mAuth: FirebaseAuth
+        // [END declare_auth]
+        private lateinit var googleSignInClient: GoogleSignInClient
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +43,20 @@ class CameraFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        googleSignInClient = GoogleSignIn.getClient(Companion.context, gso)
+        mAuth = FirebaseAuth.getInstance()
     }
+
+//    override fun onStart() {
+//        super.onStart()
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        val currentUser = mAuth.currentUser
+//
+//    }
 
     override fun onStop() {
         super.onStop()
@@ -47,4 +75,9 @@ class CameraFragment : Fragment() {
         var password : EditText = view.findViewById(R.id.password)
 
     }
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
 }
